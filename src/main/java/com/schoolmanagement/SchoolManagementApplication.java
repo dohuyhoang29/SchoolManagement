2ccfe71d733d0463ec7b0cc91b523040bda73b68
hoang.dh <hoangdohuy29092002@gmail.com>
2022-02-22 10:11:49 +0700

    test

--- src/main/java/com/schoolmanagement/SchoolManagementApplication.java
+++ src/main/java/com/schoolmanagement/SchoolManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagementApplication {
-
+//test commit
   public static void main(String[] args) {
     SpringApplication.run(SchoolManagementApplication.class, args);
   }
